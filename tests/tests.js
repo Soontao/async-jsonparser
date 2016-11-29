52ed8e6f7e93b98bab52c6b07ab1a5ae3e45bf4a
suntao <mrls@live.cn>
2016-11-29 17:19:29 +0800

    modify test

--- tests/tests.js
+++ tests/tests.js
@@ -2,23 +2,75 @@ var assert = require('assert');
 var aJSON = require('../index');
 
 describe('Basic Tests', function() {
-  var step = 4;
-  var max = 10000;
   var testContentValue = 'testvalue';
-  
-  for (var testSize = 1; testSize <= max; testSize *= step) {
-    describe(`${testSize} size object`, function() {
-      it(`can access obj[${testSize-1}] and value is correct`, function(done) {
-        aJSON.parse(genObjectJSON(testSize, testContentValue), function(err, obj) {
-          if (err) done(err);
-          else {
-            assert.equal(testContentValue, obj[testSize - 1]);
-            done();
-          }
-        })
-      });
+
+  describe(`deffrent size object`, function() {
+    it(`size 1 obj`, function(done) {
+      aJSON.parse(genObjectJSON(1, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(1, Object.keys(obj).length)
+          assert.equal(testContentValue, obj[0]);
+          done();
+        }
+      })
     });
-  }
+
+    it(`size 10 obj`, function(done) {
+      aJSON.parse(genObjectJSON(10, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(testContentValue, obj[9]);
+          done();
+        }
+      })
+    });
+
+    it(`size 100 obj`, function(done) {
+      aJSON.parse(genObjectJSON(100, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(testContentValue, obj[99]);
+          done();
+        }
+      })
+    });
+
+    it(`size 1000 obj`, function(done) {
+      aJSON.parse(genObjectJSON(1000, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(testContentValue, obj[999]);
+          done();
+        }
+      })
+    });
+
+
+
+    it(`size 10000 obj`, function(done) {
+      aJSON.parse(genObjectJSON(10000, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(testContentValue, obj[9999]);
+          done();
+        }
+      })
+    });
+
+    it(`size 100000 obj`, function(done) {
+      aJSON.parse(genObjectJSON(100000, testContentValue), function(err, obj) {
+        if (err) done(err);
+        else {
+          assert.equal(100000, Object.keys(obj).length)
+          assert.equal(testContentValue, obj[99999]);
+          done();
+        }
+      })
+    });
+
+
+  });
 
 
 });
